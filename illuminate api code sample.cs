434f40c6641845b240e8799b624f3a88d56484cf
Illuminate Insight <tim.gibbons@igdev.co.uk>
2017-05-01 14:00:01 +0100

    Add files via upload

---
+++ illuminate api code sample.cs
@@ -0,0 +1,29 @@
+            //string sParameters = "{\"username\":\"colin.wago@igdev.co.uk\", \"password\": \"lqa38vgvg\"}";
+            //string s = API_Test.IWS.IWSResponse("login", sParameters) as string;
+
+            //sParameters = "{\"userkey\":\"" + sUserKey + "\", \"supplierorderid\":\"602313\"}";
+            //sParameters = "{\"userkey\":\"" + sUserKey + "\", \"status\":\"Open\"}";
+            //sParameters = "{\"userkey\":\"" + sUserKey + "\", \"status\": \"Closed\"}";
+            //string sParameters = "{\"userkey\":\"" + sUserKey + "\", \"supplierorderid\": \"602324\"}";
+            //string s = API_Test.IWS.IWSResponse("purchaseorder", sParameters) as string;
+
+            //sParameters = "{\"userkey\":\"7157052330137238\", \"supplierorderid\":\"602320\"}";
+            //string s = API_Test.IWS.IWSResponse("createpo", sParameters) as string;
+
+            //sParameters = "{\"userkey\":\"" + sUserKey + "\", \"supplierorderid\":\"602244\", \"items\":[{productcode:712902,qtyin:24},{productcode:713005,qtyin:55}]}";
+           
+            //string sParameters = "{\"userkey\":\"" + sUserKey + "\", \"listname\":\"supplierlist\"}";
+            //string s = API_Test.IWS.IWSResponse("systemlist", sParameters) as string;
+
+            //sParameters = "{\"userkey\":\"" + sUserKey + "\"}";
+            //string s = API_Test.IWS.IWSResponse("customertypelist", sParameters) as string;
+
+            //sParameters = "{\"userkey\":\"" + sUserKey + "\", \"customertype\":\"Consumer\", \"searchtype\":\"billing\", \"soundex\":false, \"searchfields\":[{\"fieldname\":\"Forename\", \"fieldvalue\":\"T*\"}, {\"fieldname\":\"Surname\", \"fieldvalue\":\"Stockard\"}]}";
+            //sParameters = "{\"userkey\":\"" + sUserKey + "\", \"customertype\":\"Consumer\", \"searchtype\":\"billing\", \"soundex\":true, \"searchfields\":[{\"fieldname\":\"Postcode\", \"fieldvalue\":\"BH21 3QX\"}]}";
+            //string s = API_Test.IWS.IWSResponse("customersearch", sParameters) as string;
+            
+            //sParameters = "{\"userkey\":\"" + sUserKey + "\", \"customerid\":\"682025\"}";
+            //string s = API_Test.IWS.IWSResponse("customerdetails", sParameters) as string;
+
+            //sParameters = "{\"userkey\":\"" + sUserKey + "\"}";
+            //string s = API_Test.IWS.IWSResponse("folders", sParameters) as string;
